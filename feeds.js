cf930fe01d077c699158bc6fe349d29b580ef87b
Rahul Singh <dr@ph4nt0m.com>
2021-05-06 21:03:32 +0530

    uncommented other feeds

--- feeds.js
+++ feeds.js
@@ -1,184 +1,184 @@
 module.exports = [{
-  //   title: 'Dark Reading',
-  //   home: 'https://www.darkreading.com',
-  //   rss: 'https://www.darkreading.com/rss_simple.asp'
-  // },
-  // {
-  //   title: 'Krebs on Security',
-  //   home: 'https://krebsonsecurity.com',
-  //   rss: 'https://krebsonsecurity.com/feed/'
-  // },
-  // {
-  //   title: 'Threatpost',
-  //   home: 'https://threatpost.com',
-  //   rss: 'https://threatpost.com/feed/'
-  // },
-  // {
-  //   title: 'Naked Security',
-  //   home: 'https://nakedsecurity.sophos.com',
-  //   rss: 'https://nakedsecurity.sophos.com/feed/'
-  // },
-  // {
-  //   title: 'WeLiveSecurity',
-  //   home: 'https://www.welivesecurity.com',
-  //   rss: 'https://www.welivesecurity.com/feed/'
-  // },
-  // {
-  //   title: 'McAfee Blog',
-  //   home: 'https://www.mcafee.com/blogs',
-  //   rss: 'https://www.mcafee.com/blogs/feed'
-  // },
-  // {
-  //   title: 'Schneier on Security',
-  //   home: 'https://www.schneier.com',
-  //   rss: 'https://www.schneier.com/feed/atom/'
-  // },
-  // {
-  //   title: 'Quick Heal Blog',
-  //   home: 'https://blogs.quickheal.com',
-  //   rss: 'https://blogs.quickheal.com/feed/'
-  // },
-  // {
-  //   title: 'Google Online Security Blog',
-  //   home: 'https://security.googleblog.com',
-  //   rss: 'https://feeds.feedburner.com/GoogleOnlineSecurityBlog'
-  // },
-  // {
-  //   title: 'Data and computer security | The Guardian',
-  //   home: 'https://www.theguardian.com/technology/data-computer-security',
-  //   rss: 'https://www.theguardian.com/technology/data-computer-security/rss'
-  // },
-  // {
-  //   title: 'Graham Cluley',
-  //   home: 'https://grahamcluley.com',
-  //   rss: 'https://grahamcluley.com/feed/'
-  // },
-  // {
-  //   title: 'Security on TechRepublic',
-  //   home: 'http://techrepublic.com/topic/security',
-  //   rss: 'https://www.techrepublic.com/rssfeeds/topic/security/'
-  // },
-  // {
-  //   title: 'Trend Micro Blog',
-  //   home: 'https://blog.trendmicro.com',
-  //   rss: 'https://blog.trendmicro.com/feed/'
-  // },
-  // {
-  //   title: 'CIO Security',
-  //   home: 'https://www.cio.com/category/security',
-  //   rss: 'https://www.cio.com/category/security/index.rss'
-  // },
-  // {
-  //   title: 'The Hacker News',
-  //   home: 'https://thehackernews.com',
-  //   rss: 'https://feeds.feedburner.com/TheHackersNews'
-  // },
-  // {
-  //   title: 'IT Security Central - Teramind Blog',
-  //   home: 'https://itsecuritycentral.teramind.co',
-  //   rss: 'https://itsecuritycentral.teramind.co/feed/'
-  // },
-  // {
-  //   title: 'SANS Institute Security Awareness Tip of the Day',
-  //   home: 'https://www.sans.org/tip-of-the-day',
-  //   rss: 'https://www.sans.org/tip-of-the-day/rss'
-  // },
-  // {
-  //   title: 'E Hacking News',
-  //   home: 'https://www.ehackingnews.com',
-  //   rss: 'https://www.ehackingnews.com/feeds/posts/default?alt=rss'
-  // },
-  // {
-  //   title: 'SecurityWeek',
-  //   home: 'https://www.securityweek.com',
-  //   rss: 'https://feeds.feedburner.com/securityweek'
-  // },
-  // {
-  //   title: 'Help Net Security',
-  //   home: 'https://www.helpnetsecurity.com',
-  //   rss: 'https://www.helpnetsecurity.com/feed/'
-  // },
-  // {
-  //   title: 'HackRead',
-  //   home: 'https://www.hackread.com',
-  //   rss: 'https://www.hackread.com/feed/'
-  // },
-  // {
-  //   title: 'SearchSecurity',
-  //   home: 'https://searchsecurity.techtarget.com',
-  //   rss: 'https://searchsecurity.techtarget.com/rss/Security-Wire-Daily-News.xml'
-  // },
-  // {
-  //   title: 'Comodo News and Internet Security Information',
-  //   home: 'https://blog.comodo.com',
-  //   rss: 'https://blog.comodo.com/feed/'
-  // },
-  // {
-  //   title: 'Techworm',
-  //   home: 'https://www.techworm.net/category/security-and-hacking-news',
-  //   rss: 'https://www.techworm.net/category/security-and-hacking-news/feed'
-  // },
-  // {
-  //   title: 'GBHackers On Security',
-  //   home: 'https://gbhackers.com',
-  //   rss: 'https://gbhackers.com/feed/'
-  // },
-  // {
-  //   title: 'Security Gladiators',
-  //   home: 'https://securitygladiators.com/internet-security-news',
-  //   rss: 'https://securitygladiators.com/internet-security-news/feed/'
-  // },
-  // {
-  //   title: 'Information Security Buzz',
-  //   home: 'https://www.informationsecuritybuzz.com',
-  //   rss: 'https://www.informationsecuritybuzz.com/feed/'
-  // },
-  // {
-  //   title: 'Hacker Combat',
-  //   home: 'https://hackercombat.com',
-  //   rss: 'https://hackercombat.com/feed/'
-  // },
-  // {
-  //   title: 'Heimdal Security Blog',
-  //   home: 'https://heimdalsecurity.com/blog',
-  //   rss: 'https://heimdalsecurity.com/blog/feed/'
-  // },
-  // {
-  //   title: 'The Daily Swig',
-  //   home: 'https://portswigger.net/daily-swig',
-  //   rss: 'https://portswigger.net/daily-swig/rss'
-  // },
-  // {
-  //   title: 'NIST',
-  //   home: 'https://www.nist.gov/blogs/cybersecurity-insights',
-  //   rss: 'https://www.nist.gov/blogs/cybersecurity-insights/rss.xml'
-  // },
-  // {
-  //   title: 'ZDI: Upcoming Advisories',
-  //   home: 'https://www.zerodayinitiative.com',
-  //   rss: 'https://www.zerodayinitiative.com/rss/upcoming/'
-  // },
-  // {
-  //   title: 'ZDI: Published Advisories',
-  //   home: 'https://www.zerodayinitiative.com',
-  //   rss: 'https://www.zerodayinitiative.com/rss/published/'
-  // },
-  // {
-  //   title: 'ZDI: Blog',
-  //   home: 'https://www.zerodayinitiative.com/blog/',
-  //   rss: 'https://www.zerodayinitiative.com/blog/?format=rss'
-  // },
-  // {
-  //   title: 'Exploit-DB',
-  //   home: 'https://www.exploit-db.com/',
-  //   rss: 'https://www.exploit-db.com/rss.xml'
-  // },
-  // {
-  //   title: 'CISA',
-  //   home: 'https://us-cert.cisa.gov/ncas',
-  //   rss: 'https://us-cert.cisa.gov/ncas/all.xml'
-  // },
-  // {
+    title: 'Dark Reading',
+    home: 'https://www.darkreading.com',
+    rss: 'https://www.darkreading.com/rss_simple.asp'
+  },
+  {
+    title: 'Krebs on Security',
+    home: 'https://krebsonsecurity.com',
+    rss: 'https://krebsonsecurity.com/feed/'
+  },
+  {
+    title: 'Threatpost',
+    home: 'https://threatpost.com',
+    rss: 'https://threatpost.com/feed/'
+  },
+  {
+    title: 'Naked Security',
+    home: 'https://nakedsecurity.sophos.com',
+    rss: 'https://nakedsecurity.sophos.com/feed/'
+  },
+  {
+    title: 'WeLiveSecurity',
+    home: 'https://www.welivesecurity.com',
+    rss: 'https://www.welivesecurity.com/feed/'
+  },
+  {
+    title: 'McAfee Blog',
+    home: 'https://www.mcafee.com/blogs',
+    rss: 'https://www.mcafee.com/blogs/feed'
+  },
+  {
+    title: 'Schneier on Security',
+    home: 'https://www.schneier.com',
+    rss: 'https://www.schneier.com/feed/atom/'
+  },
+  {
+    title: 'Quick Heal Blog',
+    home: 'https://blogs.quickheal.com',
+    rss: 'https://blogs.quickheal.com/feed/'
+  },
+  {
+    title: 'Google Online Security Blog',
+    home: 'https://security.googleblog.com',
+    rss: 'https://feeds.feedburner.com/GoogleOnlineSecurityBlog'
+  },
+  {
+    title: 'Data and computer security | The Guardian',
+    home: 'https://www.theguardian.com/technology/data-computer-security',
+    rss: 'https://www.theguardian.com/technology/data-computer-security/rss'
+  },
+  {
+    title: 'Graham Cluley',
+    home: 'https://grahamcluley.com',
+    rss: 'https://grahamcluley.com/feed/'
+  },
+  {
+    title: 'Security on TechRepublic',
+    home: 'http://techrepublic.com/topic/security',
+    rss: 'https://www.techrepublic.com/rssfeeds/topic/security/'
+  },
+  {
+    title: 'Trend Micro Blog',
+    home: 'https://blog.trendmicro.com',
+    rss: 'https://blog.trendmicro.com/feed/'
+  },
+  {
+    title: 'CIO Security',
+    home: 'https://www.cio.com/category/security',
+    rss: 'https://www.cio.com/category/security/index.rss'
+  },
+  {
+    title: 'The Hacker News',
+    home: 'https://thehackernews.com',
+    rss: 'https://feeds.feedburner.com/TheHackersNews'
+  },
+  {
+    title: 'IT Security Central - Teramind Blog',
+    home: 'https://itsecuritycentral.teramind.co',
+    rss: 'https://itsecuritycentral.teramind.co/feed/'
+  },
+  {
+    title: 'SANS Institute Security Awareness Tip of the Day',
+    home: 'https://www.sans.org/tip-of-the-day',
+    rss: 'https://www.sans.org/tip-of-the-day/rss'
+  },
+  {
+    title: 'E Hacking News',
+    home: 'https://www.ehackingnews.com',
+    rss: 'https://www.ehackingnews.com/feeds/posts/default?alt=rss'
+  },
+  {
+    title: 'SecurityWeek',
+    home: 'https://www.securityweek.com',
+    rss: 'https://feeds.feedburner.com/securityweek'
+  },
+  {
+    title: 'Help Net Security',
+    home: 'https://www.helpnetsecurity.com',
+    rss: 'https://www.helpnetsecurity.com/feed/'
+  },
+  {
+    title: 'HackRead',
+    home: 'https://www.hackread.com',
+    rss: 'https://www.hackread.com/feed/'
+  },
+  {
+    title: 'SearchSecurity',
+    home: 'https://searchsecurity.techtarget.com',
+    rss: 'https://searchsecurity.techtarget.com/rss/Security-Wire-Daily-News.xml'
+  },
+  {
+    title: 'Comodo News and Internet Security Information',
+    home: 'https://blog.comodo.com',
+    rss: 'https://blog.comodo.com/feed/'
+  },
+  {
+    title: 'Techworm',
+    home: 'https://www.techworm.net/category/security-and-hacking-news',
+    rss: 'https://www.techworm.net/category/security-and-hacking-news/feed'
+  },
+  {
+    title: 'GBHackers On Security',
+    home: 'https://gbhackers.com',
+    rss: 'https://gbhackers.com/feed/'
+  },
+  {
+    title: 'Security Gladiators',
+    home: 'https://securitygladiators.com/internet-security-news',
+    rss: 'https://securitygladiators.com/internet-security-news/feed/'
+  },
+  {
+    title: 'Information Security Buzz',
+    home: 'https://www.informationsecuritybuzz.com',
+    rss: 'https://www.informationsecuritybuzz.com/feed/'
+  },
+  {
+    title: 'Hacker Combat',
+    home: 'https://hackercombat.com',
+    rss: 'https://hackercombat.com/feed/'
+  },
+  {
+    title: 'Heimdal Security Blog',
+    home: 'https://heimdalsecurity.com/blog',
+    rss: 'https://heimdalsecurity.com/blog/feed/'
+  },
+  {
+    title: 'The Daily Swig',
+    home: 'https://portswigger.net/daily-swig',
+    rss: 'https://portswigger.net/daily-swig/rss'
+  },
+  {
+    title: 'NIST',
+    home: 'https://www.nist.gov/blogs/cybersecurity-insights',
+    rss: 'https://www.nist.gov/blogs/cybersecurity-insights/rss.xml'
+  },
+  {
+    title: 'ZDI: Upcoming Advisories',
+    home: 'https://www.zerodayinitiative.com',
+    rss: 'https://www.zerodayinitiative.com/rss/upcoming/'
+  },
+  {
+    title: 'ZDI: Published Advisories',
+    home: 'https://www.zerodayinitiative.com',
+    rss: 'https://www.zerodayinitiative.com/rss/published/'
+  },
+  {
+    title: 'ZDI: Blog',
+    home: 'https://www.zerodayinitiative.com/blog/',
+    rss: 'https://www.zerodayinitiative.com/blog/?format=rss'
+  },
+  {
+    title: 'Exploit-DB',
+    home: 'https://www.exploit-db.com/',
+    rss: 'https://www.exploit-db.com/rss.xml'
+  },
+  {
+    title: 'CISA',
+    home: 'https://us-cert.cisa.gov/ncas',
+    rss: 'https://us-cert.cisa.gov/ncas/all.xml'
+  },
+  {
     title: 'FireEye Blogs',
     home: 'https://www.fireeye.com/blog.html',
     rss: 'https://www.fireeye.com/blog/feed'
